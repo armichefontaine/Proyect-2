ebd628412abed6482a5cc62dd21bcdfe4f94ff06
armiche <serpica.sa@hotmail.com>
2023-03-12 15:59:04 +0000

    segundograbado

--- serpica/routes/corredera.js
+++ serpica/routes/corredera.js
@@ -1,14 +1,14 @@
 const router = require('express').Router()
 
-const { addMando, addManual, getAllCorrederas, getOneCorredera, createCorredera, updateCorredera, deleteCorredera } = require('../controllers/corredera')
+const { /*addManual,*/ getAllCorrederas, getOneCorredera, createCorredera, updateCorredera, deleteCorredera } = require('../controllers/corredera')
 const {addCremallera} =require('../controllers/corredera')
 router.get('/', getAllCorrederas)
 router.get('/:id', getOneCorredera)
 router.post('/', createCorredera)
 router.put('/:id', updateCorredera)
-router.put('/:id/manual',addManual)
-router.put('/:correderaId/cremallera/:cremalleraId',addCremallera)
-router.put('/:id/mando',addMando)
+// router.put('/:id/manual',addManual)
+//router.put('/:correderaId/cremallera/:cremalleraId',addCremallera)
+// router.put('/:id/mando',addMando)
 router.delete('/:id', deleteCorredera)
 
 
--- serpica/routes/mando.js
+++ serpica/routes/mando.js
@@ -1,11 +1,11 @@
 const router = require('express').Router()
 
-const { addMando, getAllMandos, getOneMando, createMando, updateMando, deleteMando} = require('../controllers/mando.js')
+const { /*addMando,*/ getAllMandos, getOneMando, createMando, updateMando, deleteMando} = require('../controllers/mando.js')
 
 router.get('/', getAllMandos)
 router.get('/:id', getOneMando)
 router.post('/', createMando)
-router.post('/:mandoId/corredera/:correderaId',addMando)
+// router.post('/:mandoId/corredera/:correderaId',addMando)
 router.put('/:id', updateMando)
 router.delete('/:id', deleteMando)
 
